16d5188b281ea300e7bd043079d43f44bc0907d4
sofi08belen <sofiabelenbr1208@gmail.com>
2025-06-17 13:16:05 -0400

    cam,bios de sofi

---
+++ bd.sql
@@ -0,0 +1,10 @@
+create database u178928053_examen /*esto es el nombre directro de la base de datos y su credencial*/
+use u178928053_examen
+
+
+
+CREATE TABLE publicaciones (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  titulo VARCHAR(255),
+  contenido TEXT
+);
