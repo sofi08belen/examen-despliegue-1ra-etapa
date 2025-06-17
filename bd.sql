4b7a82a4bd9028c094d5096edf40ed07dfc1a737
Agosvallory <agostina@gmail.com>
2025-06-17 15:42:32 -0400

    Actualización base de datos

--- bd.sql
+++ bd.sql
@@ -9,3 +9,10 @@ CREATE TABLE publicaciones (
   contenido TEXT
 );
 
+
+CREATE TABLE usuarios( 
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    contrasenha VARCHAR(100)NOT NULL
+);
